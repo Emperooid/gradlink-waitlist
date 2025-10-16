b871a4bfaac58b256a3fcdc4dd15d48bba42ddd9
Emperooid <awosikaemmanueldefirst@gmail.com>
2025-10-16 08:01:52 +0100

    list update

--- app/api/waitlist/mail-resend.ts
+++ app/api/waitlist/mail-resend.ts
@@ -1,10 +1,10 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend('re_gMtFoktu_Je1EvAYP6uziZYmdKXhbQraD');
 
-resend.emails.send({
-  from: 'onboarding@resend.dev',
-  to: 'awosikaemmanueldefirst@gmail.com',
-  subject: 'Hello World',
-  html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-});
+
+// resend.emails.send({
+//   from: 'onboarding@resend.dev',
+//   to: 'awosikaemmanueldefirst@gmail.com',
+//   subject: 'Hello World',
+//   html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+// });
